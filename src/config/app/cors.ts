d5075961d3e4aae4ecb160057f3476969e92ea79
Paul Elisha <ireajao@yahoo.com>
2026-07-03 05:22:36 +0100

    auto commit

--- src/config/app/cors.ts
+++ src/config/app/cors.ts
@@ -4,7 +4,7 @@ import Env from "@/env.ts";
 import cors from "cors";
 
 export default cors({
- origin: Env.CORS_ORIGIN,
+ origin: ["https://6a285a07cdd9d108e8caabd4.base44.app", Env.CORS_ORIGIN],
  methods: ["GET", "POST", "PUT", "DELETE"],
  credentials: true,
 });
