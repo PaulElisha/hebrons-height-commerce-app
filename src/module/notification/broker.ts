f2a380b2e4b8d2ee4fe0c2d4ac9843a0f4b41ee5
Paul Elisha <ireajao@yahoo.com>
2026-09-14 22:10:01 +0100

    Refactor notification event structure: Updated event property names for consistency, changing 'eventType' to 'event_type'. Enhanced subscription handling to omit userId and streamline data processing in the notification broker.

--- src/module/notification/broker.ts
+++ src/module/notification/broker.ts
@@ -1,22 +1,28 @@
 /** @format */
-import { filter, Observable, Subject } from "rxjs";
+import { filter, map, Observable, Subject } from "rxjs";
 
 export interface BrokerEvent {
  userId: string;
- eventType: string;
+ event_type: string;
  data: unknown;
 }
 
 class NotificationBroker {
  private notificationTopic$ = new Subject<BrokerEvent>();
 
- public publish(userId: string, data: unknown, eventType: string) {
-  this.notificationTopic$.next({ userId, data, eventType });
+ public publish(userId: string, data: unknown, event_type: string) {
+  this.notificationTopic$.next({ userId, data, event_type });
  }
 
  public subscribe(userId: string): Observable<Omit<BrokerEvent, "userId">> {
   return this.notificationTopic$.pipe(
    filter((event) => event.userId === userId),
+   map(
+    (update): Omit<BrokerEvent, "userId"> => ({
+     event_type: update.event_type,
+     data: update.data,
+    }),
+   ),
   );
  }
 }
